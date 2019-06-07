a756606d95dc2864f5246ca77c9b0e0d144f2b76
Marisa Quijano <miss.quijano@gmail.com>
2019-06-07 15:47:26 -0400

    starting Javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Javascript starts here
+$(document).ready(function()){
+    
+}
+// don't forget to console.log EVERTHING
+
+// remember to make sure what you're putting in here for the questions goes to html
+
+// don't create something here without making sure there's a value for it in html
+
