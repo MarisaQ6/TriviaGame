9c8054dc3672b94f54e6529d06c5d7ee93151494
Marisa Quijano <miss.quijano@gmail.com>
2019-06-08 11:54:47 -0400

    fixing syntax errors in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,39 @@
 // Javascript starts here
-$(document).ready(function()){
+// $(document).ready(function()){
 
     // set up game var and questions
 
     var game = {
         questions: [
             {
-                question 1: 'What is the real name of the machete wielding killer from Camp Crystal?'
-                
-                possible answers: ["Jack Torrance", "Freddy Krueger", "John Kramer", "Jason Voorhees", "George Lutz"],
-                id: 'question-one';
+                question_One: 'What is the real name of the machete wielding killer from Camp Crystal?',
+                possibleAnswers: ["Jack Torrance", "Freddy Krueger", "John Kramer", "Jason Voorhees", "George Lutz"],
+                id: "question-one", 
                 answer: 3
-            }, {
-
-                question 2: 'In what sleepy town was famed hotel, The Overlook, in the 1980 film "The Shining?"'
-                
-                possible answers: ["Boulder, CO", "Denver, CO", "Nightfall, CO", "Estes Park, CO", "Sidewinder, CO"]
-                id: 'question-two';
+            }, 
+            {
+                question_Two: 'In what sleepy town was famed hotel, The Overlook, in the 1980 film "The Shining?"',
+                possibleAnswers: ["Boulder, CO", "Denver, CO", "Nightfall, CO", "Estes Park, CO", "Sidewinder, CO"],
+                id: 'question-two',
                 answer: 4
-
-                question 3: 'Who were the original killers behind the iconic "Ghostface" mask in the Scream franchise?'
-                
-                possible answers: ["Kieran & Hester", "Billy & Stu", "Sidney & Gale", "Cotton & Dewey ", "Drew & Neve"]
-                id: 'question-three';
+            },
+            {
+                question_Three: 'Who were the original killers behind the iconic "Ghostface" mask in the Scream franchise?',
+                possibleAnswers: ["Kieran & Hester", "Billy & Stu", "Sidney & Gale", "Cotton & Dewey ", "Drew & Neve"],
+                id: 'question-three',
                 answer: 1
-
-                question 4: 'What is the name of the eternal little girl in "Interview with the Vampire"?'
-                
-                possible answers: ["Claudia", "Monica", "Margarette", "Claudette", "Bridgette"]
-                id: 'question-four';
+            {}
+                question_Four: 'What is the name of the eternal little girl in "Interview with the Vampire"?',
+                possibleAnswers: ["Claudia", "Monica", "Margarette", "Claudette", "Bridgette"],
+                id: 'question-four',
                 answer: 0
-
-                question 5: 'What was the name of the killer in the psychological thriller "Silence of the Lambs?"'
-                
-                possible answers: ["Clarice Starling", "Jack Crawford", "Buffalo Bill", "Hannibal Lector", "Precious"]
-                id: 'question-five';
+            },
+            {
+                question_Five: 'What was the real name of the killer in the psychological thriller "Silence of the Lambs?"',
+                possibleAnswers: ["Clarice Starling", "Jack Crawford", "Buffalo Bill", "Hannibal Lector", "Precious"],
+                id: 'question-five',
                 answer: 2
-
+            },
 
                 
                 
@@ -58,38 +55,42 @@ $(document).ready(function()){
         ]
 
     }
-    console.log ("horror", random)
+    
+    function showQuestions(){
+          for (i = 0; i < game.questions.length; i++) { 
+          console.log(game.questions[i])
+        }
+    };
+    // console.log ("horror", random)
     // don't forget to console.log EVERTHING
 
 // remember to make sure what you're putting in here for the questions goes to html
-    var 
-        console.log
-    var
-        console.log
-    var
-        console.log
-    var
-        console.log
-    var
-            console.log
+    // var 
+        // console.log
+    // var
+        // console.log
+    // var
+        // console.log
+    // var
+        // console.log
+    // var
+            // console.log
 
 // don't create something here without making sure there's a value for it in html
 
 // set up parameters for trivia
 
-    var 
-   console.log("", );
-    var =
-    console.log()
-    var =
-    console.log
-    $("").text();
-    $("#").text();
+    // var 
+//    console.log("", );
+    // var =
+    // console.log()
+    // var =
+    // console.log
+    // $("").text();
+    // $("#").text();
 
-function reset (){
-    Random=Math.floor();
+// function reset (){
+    // Random=Math.floor();
 
-userTotal=
-$("totalScore").text()
-
-}
+// userTotal=
+// $("totalScore").text()
